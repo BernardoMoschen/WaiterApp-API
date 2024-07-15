cff0e89829bf002bbf26ab56a38ea192d2a46c65
BernardoMoschen <bernardomoschen.dev@gmail.com>
2024-07-15 14:51:18 -0300

    chore: mongo db

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
 
-const app = express();
+mongoose
+    .connect("mongodb://localhost:27017")
+    .then(() => {
+        console.log("🗄 - Connected to mongo");
+        const app = express();
+        const port = 3001;
+        app.listen(port, () => {
+            console.log(`🚀 - Server is running on http://localhost:${port}`);
+        });
+    })
+    .catch(() => console.log("failed to connect to mongo"));
 
-app.listen(3001, () => {
-    console.log("Server is running on http://localhost:3001");
-});
+
