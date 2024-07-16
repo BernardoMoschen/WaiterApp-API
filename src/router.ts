19330155c2481fd53600d622fee32619dc796bbb
BernardoMoschen <bernardomoschen.dev@gmail.com>
2024-07-16 18:27:48 -0300

    wip: router

--- src/router.ts
+++ src/router.ts
@@ -1,38 +1,27 @@
 import { Router } from "express";
 import { CategoryControllerInstance } from "./app/Controllers/CategoriesController/CategoriesController";
 import { ProductsControllerInstance } from "./app/Controllers/ProductsController/ProductsController";
+import { OrderControllerInstance } from "./app/Controllers/OrderController/OrderController";
 
 export const router = Router();
 
 router.get("/categories", CategoryControllerInstance.index);
+router.get("/categories/:categoryId", CategoryControllerInstance.show);
 router.post("/categories", CategoryControllerInstance.store);
-// router.get("/categories/:categoryId", (req, res) => {
-//     res.send("Ok");
-// });
-// router.get("/categories/:categoryId", (req, res) => {
-//     res.send("Ok");
-// });
+router.patch("/categories/:categoryId", CategoryControllerInstance.store);
 
 router.get("/products", ProductsControllerInstance.index);
+router.get("/products/:productId", ProductsControllerInstance.show);
 router.post("/products", ProductsControllerInstance.store);
+router.patch("/products/:productId", ProductsControllerInstance.update);
 
-
-
-
-
-
-// router.get("/orders", (req, res) => {
-//     res.send("Ok");
-// });
-
-// router.post("/orders", (req, res) => {
-//     res.send("Ok");
-// });
-
-// router.patch("/orders/:orderId", (req, res) => {
-//     res.send("Ok");
-// });
-
+router.get("/orders", OrderControllerInstance.index);
+router.get("/orders/:orderId", OrderControllerInstance.show);
+router.post("/orders", OrderControllerInstance.store);
+router.patch("/orders/:orderId", OrderControllerInstance.update);
 // router.delete("/orders/:orderId", (req, res) => {
 //     res.send("Ok");
 // });
+
+
+
